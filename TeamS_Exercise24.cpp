e99fc89442ec85ba22b8a55e461eec3863697b8e
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2022-01-14 17:14:32 -0800

    Week 2 Ex 2.4

---
+++ TeamS_Exercise24.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double one = 1.000;
+    double two = 1.414;
+    double three = 1.732;
+    double four = 2.000;
+    double five = 2.236;
+    
+    cout << "N\tSquare Root" << endl;
+    cout << "=\t===========" << endl;
+    cout << "1\t" << one << endl;
+    cout << "2\t" << two << endl;
+    cout << "3\t" << three << endl;
+    cout << "4\t" << four << endl;
+    cout << "5\t" << five << endl;
+}
