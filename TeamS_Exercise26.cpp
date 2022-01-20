68db32b79ee2adddf5bf39140d675cab510453c0
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2022-01-19 21:04:22 -0800

    Finish ex2.6

---
+++ TeamS_Exercise26.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+    int numStocks = 600;
+    double pricePerShare = 21.77;
+    double commission = 0.02;
+
+    double stocksPrice = numStocks * pricePerShare;
+    double commissionPrice = stocksPrice * commission;
+    double total = stocksPrice + commission;
+
+    cout << "Stock: $" << stocksPrice << endl;
+    cout << "Commission: $" << commissionPrice << endl;
+    cout << "Total: $" << total << endl;
+}
