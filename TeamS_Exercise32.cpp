f4694e3a6365781c127d71228d774588dd871449
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2022-01-20 23:55:27 -0800

    Do exercise 3.2

---
+++ TeamS_Exercise32.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+    int stock;
+    int purchased;
+    int sold;
+
+    cout << "What is the beginning stock? ";
+    cin >> stock;
+    cout << "What is the number purchased? ";
+    cin >> purchased;
+    cout << "What is the number sold? ";
+    cin >> sold;
+
+    cout << endl << "Ending stock: " << stock + purchased - sold;
+}
