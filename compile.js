b3105abb5deb11ccc14a4784910f37a7a9c1403e
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2022-01-30 22:32:18 -0800

    Compile only errors if error exists

--- compile.js
+++ compile.js
@@ -1,4 +1,4 @@
 require('child_process').exec(`g++ ${process.argv[2]} -o ${process.argv[2].replace(".cpp", "")}${process.platform === 'darwin' ? '.app' : ''}`,
-    (e) => {
-        console.error(e);
+    e => {
+        if(e) console.error(e)
     });
