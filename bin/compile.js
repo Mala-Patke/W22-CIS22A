780cfccb94fc5a14e575dcb01be812a9150c531a
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2022-02-01 23:27:10 -0800

    Organization

--- bin/compile.js
+++ bin/compile.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 require('child_process').exec(`g++ ${process.argv[2]} -o ${process.argv[2].replace(".cpp", "")}${process.platform === 'darwin' ? '.app' : ''}`,
     e => {
         if(e) console.error(e)
