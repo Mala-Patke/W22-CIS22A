615529a7c6df70a36e63082cff3776ab47cb6949
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2022-02-04 15:11:20 -0800

    Update scripts

--- bin/compile.js
+++ bin/compile.js
@@ -1,5 +1,2 @@
 #! /usr/bin/env node
-require('child_process').exec(`g++ ${process.argv[2]} -o ${process.argv[2].replace(".cpp", "")}${process.platform === 'darwin' ? '.app' : ''}`,
-    e => {
-        if(e) console.error(e)
-    });
+require('child_process').exec(`g++ ${process.argv[2]} -o ${process.argv[2].replace(".cpp", "")}${process.platform === 'darwin' ? '.app' : ''}`, e => e ? console.error(e) : "");
